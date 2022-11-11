e2e5b88a3170ff91b9863e62e8306cd5679e585e
CodeRomz <romualdojrb@yahoo.com>
2022-11-11 17:33:36 +0400

    First Commit

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
 import uvicorn
 from fastapi import FastAPI
 from tubea_dbcon import TubeaDbExec
-from tubea_jwt import signJWT
 
 
 app = FastAPI()
 
 # user registration and add the data in mongodb
 @app.post("/register", tags=["Register User"])
-def fa_register(user_id, user_name, user_password):
+def fa_register(user_id, user_name, user_password, user_access):
     register_user = TubeaDbExec("user")
-    register_user.add_data(user_id, user_name, user_password)
+    register_user.add_data(user_id, user_name, user_password, user_access)
     return {
         "data": "post added."
     }
@@ -39,6 +38,29 @@ def fa_login(user, password):
         return {
             "error": "Wrong login details! exept"
         }
+
+
+@app.get("/appointment", tags=["appointment"])
+def fa_view_appointment():
+    view_appointmnet = TubeaDbExec("appointment")
+
+    for data in view_appointmnet.view_all_data():
+        print(data)
+
+
+    return view_appointmnet.view_all_data()
+
+@app.get("/doctors", tags=["View List of doctors"])
+def fa_view_doctors():
+    view_doctor = TubeaDbExec("user")
+
+    for data in view_doctor.view_all_access("doctor"):
+        print(data)
+
+
+    return view_doctor.view_all_access("doctor")
+
+
 
 
 uvicorn.run(app, host="127.0.0.1", port=8000)
--- tubea_dbcon.py
+++ tubea_dbcon.py
@@ -3,7 +3,7 @@ from pymongo.server_api import ServerApi
 
 uri = "mongodb+srv://cluster0.wpk3oxf.mongodb.net/?authSource=%24external&authMechanism=MONGODB-X509&retryWrites=true&w=majority"
 
-TubeaClient = MongoClient(uri, tls=True, tlsCertificateKeyFile='X509-cert-5961867564190636061.pem',
+TubeaClient = MongoClient(uri, tls=True, tlsCertificateKeyFile='X509-cert-5961867564190636061X.pem',
                           server_api=ServerApi('1'))
 TubeaDb = TubeaClient['TechUnicorn_BEA']
 
@@ -17,18 +17,29 @@ class TubeaDbExec:
         self.user_id = user_id
         self.view_user_data = self.collection.find_one({"_id": self.user_id})
         return self.view_user_data
+
     def verify_login_data(self, user_name):
         self.user_name = user_name
         self.view_user_data = self.collection.find_one({"user_name": self.user_name})
         return self.view_user_data
 
+    def view_all_data(self):
+        db_all_data = list(self.collection.find())
+        return db_all_data
+
+    def view_all_access(self, user_access):
+        self.user_access = user_access
+        db_all_data = list(self.collection.find({"user_access": self.user_access}, {'_id': 1, 'user_name': 1}))
+        return db_all_data
+
     # add/post data in mongo
-    def add_data(self, user_id, user_name, user_password):
+    def add_data(self, user_id, user_name, user_password, user_access):
         self.collection.insert_one(
             {
             '_id': user_id,
             'user_name': user_name,
-            'user_password': user_password
+            'user_password': user_password,
+            'user_access': user_access
         }
         )
 
